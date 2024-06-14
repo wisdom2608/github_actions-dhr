100050b5fb2e78f4fae9346abe2ba1f643e3d493
wisdom <mueatech087@gmail.com>
2024-06-14 13:11:31 +0100

    update version2 file

--- version2.sh
+++ version2.sh
@@ -52,7 +52,7 @@ done
 
 #Fetch the highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION='git describe --abbrev=0 --tags 2>/dev/null'
+$CURRENT_VERSION='git describe --abbrev=0 --tags 2>/dev/null'
 
 if [[ $CURRENT_VERSION == '' ]]
 then 
